13331d8625177984940c81b71fad885877fc2824
zacharyhampton <zhampton7131@dysartstudents.org>
2024-09-29 01:34:39 -0700

    - structure change

---
+++ backend/api/core/scraping_modules/proxies.py
@@ -0,0 +1,9 @@
+import os
+
+proxies = [
+    {
+        "http": 'http://' + ':'.join(proxy.split(':')[2:]) + "@" + ':'.join(proxy.split(':')[:-2]),
+        "https": 'http://' + ':'.join(proxy.split(':')[2:]) + "@" + ':'.join(proxy.split(':')[:-2])
+    }
+    for proxy in open("../../../proxies.txt").read().splitlines()
+]
--- backend/scraping_modules/proxies.py
+++
@@ -1,9 +0,0 @@
-import os
-
-proxies = [
-    {
-        "http": 'http://' + ':'.join(proxy.split(':')[2:]) + "@" + ':'.join(proxy.split(':')[:-2]),
-        "https": 'http://' + ':'.join(proxy.split(':')[2:]) + "@" + ':'.join(proxy.split(':')[:-2])
-    }
-    for proxy in open("../proxies.txt").read().splitlines()
-]
