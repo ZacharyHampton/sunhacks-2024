d6161e4e364f7e21109f23756d9e9956b7a50f7e
aaron <dantesante2903@gmail.com>
2024-09-28 22:47:47 -0700

    routing at the expense of the entire project

---
+++ frontend/sunhacks/src/pages/ChatBox.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+<p>asdfd</p>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/sunhacks/src/pages/ProductPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<p>hsadfdsafi</p>
+</template>
+
+<style scoped>
+
+</style>
